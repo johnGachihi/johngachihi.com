5be5e04a531417d87e382a5caa00513679aa44e3
johnGachihi <johngachihi3@gmail.com>
2022-03-17 19:05:24 +0300

    add sanity with project schema

---
+++ sanity/schemas/project.js
@@ -0,0 +1,72 @@
+export default {
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: "slug",
+            options: {
+                source: 'title',
+                maxLength: '96'
+            }
+        },
+        {
+            name: 'startedAt',
+            title: 'Date Started',
+            type: 'datetime',
+        },
+        {
+            name: 'showcaseMedia',
+            title: 'Showcase Media',
+            description: 'Can only take one - image or youtube link',
+            type: 'object',
+            fields: [
+                {
+                    name: 'image',
+                    title: 'Image',
+                    type: 'image',
+                    readOnly: ({ parent, value }) => !value && parent?.youtubeLink
+                },
+                {
+                    name: 'youtubeLink',
+                    type: 'url',
+                    title: 'Youtube Link',
+                    readOnly: ({ parent, value }) => !value && parent?.image,
+                },
+            ]
+        },
+        {
+            name: 'githubLink',
+            title: 'GitHub Link',
+            type: 'url',
+        },
+        {
+            name: 'liveLink',
+            title: 'Live Link',
+            type: 'url',
+        },
+        {
+            name: 'tags',
+            title: 'Tags',
+            type: 'array',
+            of: [{ type: 'string' }]
+        },
+        {
+            name: 'shortDescription',
+            title: 'Short Description',
+            type: 'blockContent',
+        },
+        {
+            name: 'technicalDescription',
+            title: 'Technical Description',
+            type: 'blockContent'
+        }
+    ]
+}
---
+++ sanity/schemas/schema.js
@@ -0,0 +1,14 @@
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import blockContent from './blockContent'
+import project from './project'
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    project,
+
+    blockContent,
+  ]),
+})
