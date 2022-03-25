72db41b9747fd8827478ebaa08f8cfa26f48a8cf
johnGachihi <johngachihi3@gmail.com>
2022-03-25 16:36:29 +0300

    use slug to determine project data in msw handler

--- src/api/mock/handlers/project.ts
+++ src/api/mock/handlers/project.ts
@@ -1,22 +1,24 @@
-import {rest} from "msw";
-import {sanityClient} from "../../sanity-client";
+import { rest } from "msw";
+import { sanityClient } from "../../sanity-client";
 import projectSummaries from "../data/raw-project-summaries.json"
 import projects from "../data/raw-projects.json"
 
 const config = sanityClient.config()
 const sanityUrl = `https://${config.projectId}.api.sanity.io/v${config.apiVersion}*`
 
-const fetchProjectsHandler = rest.get(sanityUrl, (req, res, {json}) => {
+const fetchProjectsHandler = rest.get(sanityUrl, (req, res, { json }) => {
   const tag = req.url.searchParams.get("tag")
   if (tag === "projects")
     return res(json(projectSummaries))
 })
 
-const fetchSingleProjectHandler = rest.get(sanityUrl, (req, res, {json}) => {
+const fetchSingleProjectHandler = rest.get(sanityUrl, (req, res, { json }) => {
   const tag = req.url.searchParams.get("tag")
   if (tag === "single-project") {
-    return res(json(projects["ai-exam-invigilator"]))
-    // return res(json(projects["kotlin-symbol-processor-ksp-example"]))
+    const slug = req.url.searchParams.get("$slug")?.slice(1, -1)
+      ?? "ai-exam-invigilator"
+    // @ts-ignore
+    return res(json(projects[slug]))
   }
 })
 
