e6e6a6588f6909db729f0bd4027469ee1d5fd116
John Gachihi <johngachihi3@gmail.com>
2022-10-06 07:21:23 +0000

    use html picture to solve art direction prob of home logo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,19 @@
 import styled from "@emotion/styled";
 import Typography from "@mui/material/Typography";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import useMediaQuery from "@mui/material/useMediaQuery";
 import { Link } from "@remix-run/react";
 import { emphaticLink } from "~/styles/link";
 import logo from "../../public/images/logo.svg"
 import logoWrapped from "../../public/images/logo-wrapped.svg"
 
 export default function Index() {
-  const isScreenWidthXs = useMediaQuery("(max-width: 599px)")
-
   return (
     <Root>
       <div>
-        {isScreenWidthXs 
-           ? <Logo src={logoWrapped} style={{ width: 264, height: "auto" }}/>
-           : <Logo src={logo} style={{ width: 422 }}/>}
+        <picture>
+          <source media="(max-width: 640px)" srcSet={logoWrapped}/>
+          <img src={logo} alt="John Gachihi logo" className="mt-2 mb-10 w-[265px] sm:w-[422px]" />
+        </picture>
 
         <NavLink to="/articles">
           <Typography variant="h5">Articles</Typography>
@@ -41,12 +39,12 @@ const Root = styled.div`
     flex-direction: column;
     height: 100vh;
   
-  @media (max-width: 599px) {
+  @media (max-width: 639px) {
     padding-top: 48px;
     margin: 0 32px;
   }
   
-  @media (min-width: 600px) {
+  @media (min-width: 640px) {
     justify-content: center;
     align-items: center;
     padding-bottom: 56px;
@@ -55,11 +53,6 @@ const Root = styled.div`
   @media (max-height: 400px) {
     height: 130vh;
   } 
-`
-
-const Logo = styled.img`
-  margin-top: 8px;
-  margin-bottom: 40px
 `
 
 const NavLink = styled(Link)`
