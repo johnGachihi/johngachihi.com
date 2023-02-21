6cc48a2ce55563ec5c111f4c8d3e4b0a7a5eefdd
johnGachihi <johngachihi3@gmail.com>
2023-02-21 10:58:14 +0300

    add e2e smoke test

--- app/routes/__app/contacts.tsx
+++ app/routes/__app/contacts.tsx
@@ -1,23 +1,23 @@
-import {type MetaFunction} from "@remix-run/node";
-import {mdiGithub, mdiLinkedin, mdiEmail} from "@mdi/js";
+import { type MetaFunction } from "@remix-run/node";
+import { mdiGithub, mdiLinkedin, mdiEmail } from "@mdi/js";
 import Icon from "@mdi/react";
 import IconButton from "@mui/material/IconButton";
 
 export const meta: MetaFunction = () => {
-    return {title: "Contact"}
+    return { title: "Contact" }
 }
 
 export default function ContactsPage() {
     return (
         <div className="h-[calc(100vh-64px)] flex gap-x-2 lg:gap-x-5 justify-center items-center">
-            <IconButton color="inherit" component="a" href="https://github.com/johnGachihi" target="_blank">
-                <Icon path={mdiGithub} className="w-12"/>
+            <IconButton color="inherit" component="a" href="https://github.com/johnGachihi" target="_blank" aria-label="GitHub account">
+                <Icon path={mdiGithub} className="w-12" />
             </IconButton>
-            <IconButton color="inherit" component="a" href="https://linkedin.com/in/john-gachihi/" target="_blank">
-                <Icon path={mdiLinkedin} className="w-12"/>
+            <IconButton color="inherit" component="a" href="https://linkedin.com/in/john-gachihi/" target="_blank" aria-label="LinkedIn account">
+                <Icon path={mdiLinkedin} className="w-12" />
             </IconButton>
-            <IconButton color="inherit" component="a" href="mailto:johngachihi3@gmail.com" target="_blank">
-                <Icon path={mdiEmail} className="w-12"/>
+            <IconButton color="inherit" component="a" href="mailto:johngachihi3@gmail.com" target="_blank" aria-label="Email">
+                <Icon path={mdiEmail} className="w-12" />
             </IconButton>
         </div>
     )
--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -2,8 +2,10 @@ describe("smoke tests", () => {
   it("allows navigation", () => {
     cy.visit("/");
 
+    // Open articles page
     cy.findByRole("link", { name: /articles/i }).click();
     cy.findByRole("heading", { name: /articles/i });
+    // Open an article
     cy.findByRole("link", {
       name: /the importance of abstraction in software development/i,
     }).click();
@@ -11,10 +13,13 @@ describe("smoke tests", () => {
       name: /the importance of abstraction in software development/i,
     });
 
+    // Go back to home page
     cy.findByRole("link", { name: /john gachihi/i }).click();
 
+    // Open projects page
     cy.findByRole("link", { name: /projects/i }).click();
     cy.findByRole("heading", { name: /projects/i });
+    // Open a project
     cy.findByRole("link", {
       name: /Kotlin Symbol Processor \(KSP\) Example/i,
     }).click();
@@ -22,8 +27,29 @@ describe("smoke tests", () => {
       name: /Kotlin Symbol Processor \(KSP\) Example/i,
     });
 
+    // Go back to home page
     cy.findByRole("link", { name: /john gachihi/i }).click();
 
+    // Open contacts page
     cy.findByRole("link", { name: /contacts/i }).click();
+    cy.findByRole("link", { name: /linkedin/i });
+
+    // Open articles page using navbar link
+    cy.findByRole("navigation").within(() =>
+      cy.findByRole("link", { name: /articles/i }).click()
+    );
+    cy.findByRole("heading", { name: /articles/i });
+
+    // Open projects page using navbar link
+    cy.findByRole("navigation").within(() =>
+      cy.findByRole("link", { name: /projects/i }).click()
+    );
+    cy.findByRole("heading", { name: /projects/i });
+
+    // Open contacts page using navbar link
+    cy.findByRole("navigation").within(() =>
+      cy.findByRole("link", { name: /contacts/i }).click()
+    );
+    cy.findByRole("link", { name: /linkedin/i });
   });
 });
