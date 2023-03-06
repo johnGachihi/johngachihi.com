7a1e6bf1635dc28dd81b59d3ff18ea4b71643ba7
johnGachihi <johngachihi3@gmail.com>
2023-03-06 11:21:13 +0300

    temp fix for @mui createTheme_default error

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,20 @@
-import type {LinksFunction, MetaFunction} from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
-    Link,
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration, useCatch, useLocation, useTransition,
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration, useCatch, useLocation, useTransition,
 } from "@remix-run/react";
-import theme from "./styles/mui/theme";
 import { MuiDocumentWrapper } from "./styles/mui/setup-utils";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
-import {AppBar} from "~/components/AppBar";
+import { AppBar } from "~/components/AppBar";
 import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
-import {CircularProgress} from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import { load, trackPageview } from 'fathom-client'
 import { useEffect } from 'react'
 
@@ -31,24 +30,24 @@ export const links: LinksFunction = () => {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap",
     },
-      {
-          rel: 'apple-touch-icon',
-          sizes: '180x180',
-          href: '/favicons/apple-touch-icon.png',
-      },
-      {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '32x32',
-          href: '/favicons/favicon-32x32.png',
-      },
-      {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '16x16',
-          href: '/favicons/favicon-16x16.png',
-      },
-      {rel: 'manifest', href: '/manifest.json'},
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicons/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicons/favicon-16x16.png',
+    },
+    { rel: 'manifest', href: '/manifest.json' },
     { rel: 'icon', href: '/favicon.ico' },
   ];
 };
@@ -57,7 +56,7 @@ export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "John Gachihi",
   viewport: "width=device-width,initial-scale=1",
-  "theme-color": theme.palette.primary.main,
+  "theme-color": "#000",
   "emotion-insertion-point": "emotion-insertion-point",
 });
 
@@ -76,20 +75,20 @@ const Fathom = () => {
 }
 
 function PageLoadingMessage() {
-    const transition = useTransition()
+  const transition = useTransition()
 
-    return (
-        <Snackbar
-            open={transition.state === "loading"}
-            anchorOrigin={{horizontal: "right", vertical: "bottom"}}
-            message={
-                <div className="flex items-center gap-x-4">
-                    <CircularProgress size={24} color="inherit" />
-                    Page loading...
-                </div>
-            }
-        />
-    )
+  return (
+    <Snackbar
+      open={transition.state === "loading"}
+      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      message={
+        <div className="flex items-center gap-x-4">
+          <CircularProgress size={24} color="inherit" />
+          Page loading...
+        </div>
+      }
+    />
+  )
 }
 
 export default function App() {
@@ -116,14 +115,14 @@ export default function App() {
 }
 
 export function ErrorBoundary() {
-    return (
-        <MuiDocumentWrapper>
-          <html lang="en" className="h-full">
-          <head>
-            <Meta />
-            <Links />
-          </head>
-          <body className="flex flex-col h-full">
+  return (
+    <MuiDocumentWrapper>
+      <html lang="en" className="h-full">
+        <head>
+          <Meta />
+          <Links />
+        </head>
+        <body className="flex flex-col h-full">
           <AppBar />
           <main className="w-4/5 mx-auto grow flex flex-col items-center justify-center text-center">
             <span className="text-5xl text-red-800 max-w-2xl mb-10">Ayaya! Something's wrong here</span>
@@ -132,10 +131,10 @@ export function ErrorBoundary() {
             </Link>
           </main>
           <Scripts />
-          </body>
-          </html>
-        </MuiDocumentWrapper>
-    )
+        </body>
+      </html>
+    </MuiDocumentWrapper>
+  )
 }
 
 export function CatchBoundary() {
@@ -144,22 +143,22 @@ export function CatchBoundary() {
 
   if (caught.status === 404)
     return (
-        <MuiDocumentWrapper>
-          <html lang="en" className="h-full">
-            <head>
-              <Meta />
-              <Links />
-            </head>
-            <body className="flex flex-col h-full">
-              <AppBar />
-              <main className="w-4/5 mx-auto grow flex flex-col items-center justify-center text-center">
-                <span className="text-5xl mb-4">404</span>
-                <span className="text-xl mb-1">This path does not exist </span>
-                <span className="text-xl max-w-full truncate">{location.pathname}</span>
-              </main>
-              <Scripts />
-            </body>
-          </html>
-        </MuiDocumentWrapper>
+      <MuiDocumentWrapper>
+        <html lang="en" className="h-full">
+          <head>
+            <Meta />
+            <Links />
+          </head>
+          <body className="flex flex-col h-full">
+            <AppBar />
+            <main className="w-4/5 mx-auto grow flex flex-col items-center justify-center text-center">
+              <span className="text-5xl mb-4">404</span>
+              <span className="text-xl mb-1">This path does not exist </span>
+              <span className="text-xl max-w-full truncate">{location.pathname}</span>
+            </main>
+            <Scripts />
+          </body>
+        </html>
+      </MuiDocumentWrapper>
     )
 }
