5fd087cbeaf1e4d3606e771189d1537d10c7c9d8
johnGachihi <johngachihi3@gmail.com>
2023-03-13 18:15:55 +0300

    enable React 18

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,16 +1,31 @@
 import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./styles/mui/theme";
 import { CssBaseline } from "@mui/material";
 import { ClientEmotionCacheProvider } from "./styles/mui/setup-utils";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
 
-hydrate(
-  <ClientEmotionCacheProvider>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RemixBrowser />
-    </ThemeProvider>
-  </ClientEmotionCacheProvider>,
-  document
-);
+const hydrate = () => {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <ClientEmotionCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <RemixBrowser />
+          </ThemeProvider>
+        </ClientEmotionCacheProvider>
+      </StrictMode>
+    );
+  });
+};
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
+}
