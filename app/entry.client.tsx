6ad5e83e1017903456cc5786b868f4bba6ff3252
johnGachihi <johngachihi3@gmail.com>
2023-03-16 17:57:19 +0300

    revert to react 17 (disable react 18)
    ... until I remove emotion and mui

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,29 +3,14 @@ import { ThemeProvider } from "@emotion/react";
 import theme from "./styles/mui/theme";
 import { CssBaseline } from "@mui/material";
 import { ClientEmotionCacheProvider } from "./styles/mui/setup-utils";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { hydrate } from "react-dom";
 
-const hydrate = () => {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <ClientEmotionCacheProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <RemixBrowser />
-          </ThemeProvider>
-        </ClientEmotionCacheProvider>
-      </StrictMode>
-    );
-  });
-};
-
-if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  window.setTimeout(hydrate, 1);
-}
+hydrate(
+  <ClientEmotionCacheProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RemixBrowser />
+    </ThemeProvider>
+  </ClientEmotionCacheProvider>,
+  document
+);
