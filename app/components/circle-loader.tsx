572972e43e9bfefc008b54b1ec80ba0d2e6df4e3
John Gachihi <johngachihi3@gmail.com>
2024-01-08 20:50:13 +0300

    fix circle-loader look/animation

--- app/components/circle-loader.tsx
+++ app/components/circle-loader.tsx
@@ -6,6 +6,11 @@ export function CircleLoader({ color = "primary", className }: {
 }) {
   return (
     <div
-      className={clsx(className, `animate-spin rounded-full h-6 w-6 border-2 border-t-${color} border-r-${color} border-b-${color}/25 border-l-${color}/25`)} />
+      className={clsx(className,
+        `animate-spin rounded-full h-6 w-6 border-2`,
+        color === "primary"
+          ? "border-t-primary border-r-primary border-b-primary/25 border-l-primary/25"
+          : "border-t-onPrimary border-r-onPrimary border-b-onPrimary/25 border-l-onPrimary/25"
+      )} />
   );
 }
